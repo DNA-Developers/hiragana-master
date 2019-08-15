770309155be126bd6bf524d71d236eacb34d31b4
Dallen9 <allendonovan08@gmail.com>
2019-08-14 17:01:42 -0700

    make it better

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  menu = '../../../assets/menu.svg';
+  close = '../../../assets/close.svg';
 
+  screenHeight = window.innerHeight;
+  screenWidth = window.innerWidth;
+  show = true;
+  hidden = false;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event?) {
+    this.screenHeight = window.innerHeight;
+    this.screenWidth = window.innerWidth;
+  }
   constructor() { }
 
   ngOnInit() {
+  }
+  ngOnChanges() {
+    this.onResize();
+  }
+
+  hide() {
+    if (this.screenWidth >= 768) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  toggle() {
+    this.show = !this.show;
+    this.hidden = !this.hidden;
+  }
+
+  disappear() {
+    if (this.hidden === true) {
+      return 'none';
+    } else {
+      return 'block';
+    }
   }
 
 }
